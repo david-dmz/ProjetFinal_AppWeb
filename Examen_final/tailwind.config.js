33aa4592454217112596fc001c7262e4e4f82519
DavidMVgit-coder <2141799@cegepmv.ca>
2026-04-27 12:06:19 -0400

    ajout dark/light mode

--- Examen_final/tailwind.config.js
+++ Examen_final/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Active le mode sombre basé sur une classe
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", // Scanne tes fichiers de composants
-    "./node_modules/flowbite/**/*.js" // Indispensable : dit à Tailwind de scanner les classes de Flowbite
+    "./node_modules/flowbite/**/*.js" 
   ],
   theme: {
     extend: {},
