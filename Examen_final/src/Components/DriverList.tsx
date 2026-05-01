33fb250e181239f8f79f5056f232b7749c7e0767
DavidMVgit-coder <2141799@cegepmv.ca>
2026-04-30 21:33:44 -0400

    important des imgs de pilote dans un fichier different

--- Examen_final/src/Components/DriverList.tsx
+++ Examen_final/src/Components/DriverList.tsx
@@ -1,53 +1,8 @@
 import { useState, useEffect } from "react";
 import { fetchDrivers, type Driver } from "../API/openF1";
 import "./DriverList.css";
+import {customImages} from './DriverImages';
 
-// --- DICTIONNAIRE D'IMAGES HAUTE DÉFINITION (Grille F1) ---
-const customImages: Record<number, string> = {
-  // Red Bull Racing
-  1: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png",
-  11: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/S/SERPER01_Sergio_Perez/serper01.png",
-
-  // Ferrari
-  16: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png",
-  55: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png",
-
-  // Mercedes
-  44: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png",
-  63: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/G/GEORUS01_George_Russell/georus01.png",
-
-  // McLaren
-  4: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png",
-  81: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OSCPIA01_Oscar_Piastri/oscpia01.png",
-
-  // Aston Martin
-  14: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png",
-  18: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LANSTR01_Lance_Stroll/lanstr01.png",
-
-  // Alpine
-  10: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/P/PIEGAS01_Pierre_Gasly/piegas01.png",
-  31: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/E/ESTOCO01_Esteban_Ocon/estoco01.png",
-
-  // Williams
-  23: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/A/ALEALB01_Alexander_Albon/alealb01.png",
-  2: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LOGSAR01_Logan_Sargeant/logsar01.png",
-
-  // Visa Cash App RB (AlphaTauri)
-  22: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/Y/YUKTSU01_Yuki_Tsunoda/yuktsu01.png",
-  3: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/D/DANRIC01_Daniel_Ricciardo/danric01.png",
-
-  // Kick Sauber (Alfa Romeo)
-  77: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/V/VALBOT01_Valtteri_Bottas/valbot01.png",
-  24: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/Z/ZHOGUA01_Guanyu_Zhou/zhogua01.png",
-
-  // Haas
-  20: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/K/KEVMAG01_Kevin_Magnussen/kevmag01.png",
-  27: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/N/NICHUL01_Nico_Hulkenberg/nichul01.png",
-
-  // Pilotes de réserve récents (au cas où l'API les renvoie)
-  38: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/O/OLIbea01_Oliver_Bearman/olibea01.png", // Oliver Bearman
-  40: "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LIALAW01_Liam_Lawson/lialaw01.png", // Liam Lawson
-};
 
 export const DriverList = () => {
   const [drivers, setDrivers] = useState<Driver[]>([]);
